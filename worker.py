467e981a1df88b0a75ddfec0bf8599028bf01897
Mishra-Manit <mshmanit@gmail.com>
2025-05-31 12:30:43 -0700

    Update worker.py to use correct RQ imports

--- worker.py
+++ worker.py
@@ -1,6 +1,6 @@
 import os
 import redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 
 # Import the tasks module instead of app
 from tasks import generate_email
@@ -8,10 +8,9 @@ from tasks import generate_email
 listen = ['default']
 
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-
 conn = redis.from_url(redis_url)
+queue = Queue(connection=conn)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work() 
+    worker = Worker([queue], connection=conn)
+    worker.work() 
